0cee4225682221095a8e3d5e18f22d7ceee94070
stortor <storrtor@gmail.com>
2022-02-04 15:11:11 +0300

    classwork done

--- cart-service/src/main/java/com/stortor/spring/web/cart/configs/RestConfig.java
+++ cart-service/src/main/java/com/stortor/spring/web/cart/configs/RestConfig.java
@@ -7,9 +7,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class AppConfig {
+public class RestConfig {
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
