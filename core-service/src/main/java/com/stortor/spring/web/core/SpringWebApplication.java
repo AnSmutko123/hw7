8e068d4ed9136f184f64c6a564182071353514e0
stortor <storrtor@gmail.com>
2022-02-22 12:52:51 +0300

    ...

--- core-service/src/main/java/com/stortor/spring/web/core/SpringWebApplication.java
+++ core-service/src/main/java/com/stortor/spring/web/core/SpringWebApplication.java
@@ -2,6 +2,7 @@ package com.stortor.spring.web.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class SpringWebApplication {
